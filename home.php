7ae37c32604bc2a116e10eb9d31a8d55b5e6eb55
Manginar Sirait <102657186+suninar@users.noreply.github.com>
2022-11-24 12:14:32 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ulos Ku Etnic Fashion</title>
+	<meta name="viewport" content="width=device-width">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/all.css">
+	<link rel="stylesheet" type="text/css" href="css/all.min.css">
+	<link rel="stylesheet" type="text/css" href="css/lightbox.css">
+	<link rel="stylesheet" type="text/css" href="css/flexslider.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+	<link rel="stylesheet" type="text/css" href="css/owl.theme.default.css">
+	<link rel="stylesheet" type="text/css" href="css/jquery.rateyo.css"/>
+	<link rel="stylesheet" type="text/css" href="inner-page-style.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Raleway:400,500,600,700" rel="stylesheet">
+</head>
+<body>
+	<div id="page" class="site" itemscope itemtype="http://schema.org/LocalBusiness">
+		<header class="site-header">
+			<div class="top-header">
+				<div class="container">
+					<div class="top-header-left">
+						<div class="top-header-block">
+							<a href="mailto:info@educationpro.com" itemprop="email"><i class="fas fa-envelope"></i> uloskuetnicfashion.com</a>
+						</div>
+						<div class="top-header-block">
+							<a href="tel:+9779813639131" itemprop="telephone"><i class="fas fa-phone"></i> +977 9813639131</a>
+						</div>
+					</div>
+					<div class="top-header-right">
+						<div class="social-block">
+							<ul class="social-list">
+								<li><a href=""><i class="fab fa-viber"></i></a></li>
+								<li><a href=""><i class="fab fa-facebook-square"></i></a></li>
+								<li><a href=""><i class="fab fa-facebook-messenger"></i></a></li>
+								<li><a href=""><i class="fab fa-twitter"></i></a></li>
+							</ul>
+						</div>
+						<div class="login-block">
+							<a href="">Login /</a>
+							<a href="">Register</a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Top header Close -->
+			<div class="main-header">
+				<div class="container">
+					<div class="logo-wrap" itemprop="logo">
+						<img src="images/site-logo.jpg" alt="Logo Image">
+					</div>
+					<div class="nav-wrap">
+						<nav class="nav-desktop">
+							<ul class="menu-list">
+								<li><a href="#">Home</a></li>
+								<li class="menu-parent">Shop
+									<ul class="sub-menu">
+										<li><a href="#">Product</a></li>
+										<li><a href="#">Basket</a></li>
+									</ul>
+								</li>
+								<li class="menu-parent">Categories
+									<ul class="sub-menu">
+										<li><a href="#">Baju</a></li>
+										<li><a href="#">Dress</a></li>
+										<li><a href="#">Celana</a></li>
+										<li><a href="#">Tas</a></li>
+										<li><a href="#">Pocket</a></li>
+									</ul>
+								</li>
+								<li><a href="">About</a></li>
+								<li><a href="">Gallery</a></li>
+								<li><a href="">Contact</a></li>
+							</ul>
+						</nav>
+						<div id="bar">
+							<i class="fas fa-bars"></i>
+						</div>
+						<div id="close">
+							<i class="fas fa-times"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+
+		<div class="banner">
+			<div class="owl-four owl-carousel" itemprop="image">
+				<img src="images/page-banner.jpg" alt="Image of Bannner">
+				<img src="images/page-banner2.jpg" alt="Image of Bannner">
+				<img src="images/page-banner3.jpg" alt="Image of Bannner">
+			</div>
+			<div class="container" itemprop="description">
+				<h1>Selamat Datang di Toko Ulos KU Etnic Fashion</h1>
+				<h3>Anda dapat berbelanja fashion things dengan khaas etnik dengan nyaman dan aman di Website kami</h3>
+			</div>
+			 <div id="owl-four-nav" class="owl-nav"></div>S
+		</div>
+		
+		<div class="page-heading">
+			<div class="container">
+				<h2>POPULER PRODUCT</h2>
+			</div>
+		</div>
+		<!-- Popular courses End -->
+		<div class="learn-courses">
+			<div class="container">
+				<div class="courses">
+					<div class="owl-one owl-carousel">
+						<div class="box-wrap" itemprop="event" itemscope itemtype=" http://schema.org/Course">
+							<div class="img-wrap" itemprop="image"><img src="images/course-pic.jpg" alt="courses picture"></div>
+								<a href="#" class="learn-desining-banner" itemprop="name">Baju Dress Tenun >>></a>
+							<div class="box-body" itemprop="description">
+								<p>Baju Dress dengan desain motif etnik, dengan bentuk dan cutting yang unik. </p>
+								<section itemprop="time">
+									<p><span>Bahan :</span> Kain Tenun</p>
+									<p><span>Ukuran :</span> S,M,L,XL</p>
+									<p><span>Harga :</span> Rp800.000</p>
+								</section>
+							</div>
+						</div>
+
+						<div class="box-wrap" itemprop="event" itemscope itemtype=" http://schema.org/Course">
+							<div class="img-wrap"  itemprop="image"><img src="images/course-pic2.jpg" alt="courses picture"></div>
+								<a href="#" class="learn-desining-banner" itemprop="name">Dress Set Tenun >>></a>
+							<div class="box-body" itemprop="description">
+								<p>Baju Dress dengan desain motif etnik, dengan bentuk dan cutting atasan balon dan bawahan span.</p>
+								<section itemprop="time">
+									<p><span>Bahan :</span> Kain Tenun</p>
+									<p><span>Ukuran :</span> S,M,L,XL</p>
+									<p><span>Harga :</span> Rp1.600.000</p>
+								</section>
+							</div>
+						</div>
+
+						<div class="box-wrap" itemprop="event" itemscope itemtype=" http://schema.org/Course">
+							<div class="img-wrap"  itemprop="image"><img src="images/course-pic3.jpg" alt="courses picture"></div>
+								<a href="#" class="learn-desining-banner" itemprop="name">Celana Kulot Tenun>>></a>
+							<div class="box-body" itemprop="description">
+								<p>Celana panjang kulot dengan motif etnik, dengan detail ikat pinggang yang unik</p>
+								<section itemprop="time">
+									<p><span>Bahan :</span> Kain Tenun</p>
+									<p><span>Ukuran :</span> S,M,L,XL</p>
+									<p><span>Harga :</span> Rp650.000</p>
+								</section>
+							</div>
+						</div>
+
+						<div class="box-wrap" itemprop="event" itemscope itemtype=" http://schema.org/Course">
+							<div class="img-wrap"  itemprop="image"><img src="images/course-pic4.jpg" alt="courses picture"></div>
+								<a href="#" class="learn-desining-banner" itemprop="name">Blouse Outer Tenun  >>></a>
+							<div class="box-body" itemprop="description">
+								<p>Blouse dengan motif etnik, lengan pendek dengan cutting yang kece.</p>
+								<section itemprop="time">
+									<p><span>Bahan :</span> Kain Tenun</p>
+									<p><span>Ukuran :</span> S,M,L,XL</p>
+									<p><span>Harga :</span> Rp500.000</p>
+								</section>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Learn courses End -->
+		
+
+	</div>
+	<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="js/lightbox.js"></script>
+	<script type="text/javascript" src="js/all.js"></script>
+	<script type="text/javascript" src="js/isotope.pkgd.min.js"></script>
+	<script type="text/javascript" src="js/owl.carousel.js"></script>
+	<script type="text/javascript" src="js/jquery.flexslider.js"></script>
+	<script type="text/javascript" src="js/jquery.rateyo.js"></script>
+	<!-- <script type="text/javascript" src="js/jquery.mmenu.all.js"></script> -->
+	<!-- <script type="text/javascript" src="js/jquery.meanmenu.min.js"></script> -->
+	<script type="text/javascript" src="js/custom.js"></script>
+</body>
+</html>
